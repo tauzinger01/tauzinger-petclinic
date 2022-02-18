df804ab040e939cf08f0fba654ec61afe39b3fd2
tauzinger01 <thomas.auzinger@cognizent.com>
2022-02-17 22:06:08 -0500

    First commit

---
+++ src/main/java/com/example/tauzingerpetclinic/TauzingerPetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.tauzingerpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TauzingerPetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TauzingerPetclinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tauzingerpetclinic/TauzingerPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tauzingerpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TauzingerPetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
